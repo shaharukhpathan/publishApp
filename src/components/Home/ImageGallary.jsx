a4fdc0c609382565ce21d546530412f8771a8b56
shaharukhpathan <89965634+shaharukhpathan@users.noreply.github.com>
2021-09-24 12:41:26 +0530

    to create wikipiidia information

--- src/components/Home/ImageGallary.jsx
+++ src/components/Home/ImageGallary.jsx
@@ -5,29 +5,16 @@ export const ImageGallary = () => {
     <table>
       <tr>
         <td>
-          <ImageGallaryItem />
+          <ImageGallaryItem wikiUrl="https://en.wikipedia.org/wiki/Shah_Rukh_Khan"  imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Shah_Rukh_Khan_graces_the_launch_of_the_new_Santro.jpg/330px-Shah_Rukh_Khan_graces_the_launch_of_the_new_Santro.jpg" title="shahrukh khan" />
         </td>
         <td>
-          <ImageGallaryItem />
+          <ImageGallaryItem wikiUrl="https://en.wikipedia.org/wiki/Salman_Khan"  imgUrl="https://upload.wikimedia.org/wikipedia/commons/8/86/Salman_Khan_at_Renault_Star_Guild_Awards.jpg" title="salman khan" />
         </td>
         <td>
-          <ImageGallaryItem />
+          <ImageGallaryItem wikiUrl="https://en.wikipedia.org/wiki/Madhuri_Dixit"  imgUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Madhuri_Dixit_promoting_Total_Dhamaal_in_2019_%28cropped%29.jpg/330px-Madhuri_Dixit_promoting_Total_Dhamaal_in_2019_%28cropped%29.jpg" title="Maduri Dixit" />
         </td>
-        <td>
-          <ImageGallaryItem />
-        </td>
-        <td>
-          <ImageGallaryItem />
-        </td>
-        <td>
-          <ImageGallaryItem />
-        </td>
-        <td>
-          <ImageGallaryItem />
-        </td>
-        <td>
-          <ImageGallaryItem />
-        </td>
+        
+        
       </tr>
     </table>
   );
