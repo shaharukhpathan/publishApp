0a206cafb42fb30cb0cbf3c8f96eb0e6e3494c9c
PickupBiz <pickupyourbiz@gmail.com>
2021-09-24 12:25:42 +0530

    Props Implementation

--- src/components/Home/ImageGallaryItem.jsx
+++ src/components/Home/ImageGallaryItem.jsx
@@ -1,19 +1,15 @@
-export const ImageGallaryItem = () => {
+export const ImageGallaryItem = (props) => {
   return (
-    <a href="https://en.wikipedia.org/wiki/Shah_Rukh_Khan" target="_blank">
+    <a href={props.wikiUrl} target="_blank">
       <table>
         <tr>
           <td>
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Shah_Rukh_Khan_graces_the_launch_of_the_new_Santro.jpg/330px-Shah_Rukh_Khan_graces_the_launch_of_the_new_Santro.jpg"
-              alt="Shahrukh"
-              width="180px"
-            />
+            <img src={props.imgUrl} alt={props.title} width="180px" />
           </td>
         </tr>
         <tr>
           <td>
-            <b>Shahrukh Khan</b>
+            <b>{props.title}</b>
           </td>
         </tr>
       </table>
